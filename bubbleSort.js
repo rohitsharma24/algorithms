02c85b8dbd6116cf98692b08d5e1450e5662e831
Rohit <rohit.sharma.2495@outlook.com>
2019-11-03 23:22:50 +0530

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+const unsortedArr = [4,1,8,7,6];
+
+const bubbleSort = (arr = []) => {
+  const length = arr.length; let temp;
+  for(let i = 0; i < length; i++){
+    for(let j = 0; j < length - 1; j++){
+      if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+bubbleSort(unsortedArr);
+console.log(unsortedArr);
