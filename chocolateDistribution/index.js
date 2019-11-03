ba3996ffd426faf0fb4bf359f9013c9e8f1a00a6
Rohit <rohit.sharma.2495@outlook.com>
2019-11-04 01:50:01 +0530

    chocolate distribution problem

---
+++ chocolateDistribution/index.js
@@ -0,0 +1,15 @@
+const a = [12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50];
+const m = 7;
+
+const init = (arr, m) => {
+  arr.sort((x, y) => x - y);
+  let min = arr[arr.length - 1];
+  for (let i = 0, j = m - 1; j < arr.length; i++, j++) {
+    var diff = Math.abs(arr[i] - arr[j]);
+    if (diff < min) {
+      min = diff;
+    }
+  }
+  return min;
+};
+console.log(init(a, m));
