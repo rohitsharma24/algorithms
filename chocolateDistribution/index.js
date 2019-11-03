00dad04db33a8c2d334a7a24f15db7324f938601
Rohit <rohit.sharma.2495@outlook.com>
2019-11-04 01:56:07 +0530

    student cant greater than chocs

--- chocolateDistribution/index.js
+++ chocolateDistribution/index.js
@@ -2,6 +2,9 @@ const a = [12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50];
 const m = 7;
 
 const init = (arr, m) => {
+  if (arr.length < m) {
+    return -1;
+  }
   arr.sort((x, y) => x - y);
   let min = arr[arr.length - 1];
   for (let i = 0, j = m - 1; j < arr.length; i++, j++) {
